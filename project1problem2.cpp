972a8af63e7ba00795d4c48115fde25740c50d3f
sunnikbe <sunnikbe@uio.no>
2022-09-08 12:39:25 +0200

    forgot about changes

--- project1problem2.cpp
+++ project1problem2.cpp
@@ -2,7 +2,7 @@
 
 // Defining x vector and filling with values between 0 and 1
 int N = 100;
-arma::vec x = arma::linspace(0.0, 1.0, N);
+arma::vec x = arma::linspace(0.0, 1.0, N + 1);
 
 // declaring x_i for some reason
 double x_i;
@@ -23,18 +23,16 @@ int main(){
   int width = 12, prec = 4; // four decimals and 12 characters
 
   // function
-  double res = u(x_i); //should return our function values
+  double res[N]; //should return our function values
 
   // Loop over steps
-    for (int i = 0; i <= (N - 1); i++)
+    for (int i = 0; i <= (N); i++)
     {
+      res[i] = u(x[i]);
     // Write a line with the current x and y values (nicely formatted) to file
-    ofile << std::setw(width) << std::setprecision(prec) << std::scientific << double (x_i)
-          << std::setw(width) << std::setprecision(prec) << std::scientific << res
+    ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x[i]
+          << std::setw(width) << std::setprecision(prec) << std::scientific << res[i]
           << std::endl;
-
-    // Function
-    double x_i = x(i);
   }
 
   // closing output file
