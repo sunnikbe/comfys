d9f1dd1d2ba5a6c8349e67176d0053186c00a02a
sunnikbe <sunnikbe@uio.no>
2022-09-08 13:39:50 +0200

    Problem 2 done

--- Project1/p2.cpp
+++ Project1/p2.cpp
@@ -4,11 +4,8 @@
 int N = 100;
 arma::vec x = arma::linspace(0.0, 1.0, N + 1);
 
-// declaring x_i for some reason
-double x_i;
-
-double u(double x_i) {
-  return 1 - (1 - exp(-10)*x_i - exp(-10*x_i));
+double u(double x) {
+  return 1 - (1 - exp(-10))*x - exp(-10*x);
 }
 
 int main(){
@@ -28,7 +25,8 @@ int main(){
   // Loop over steps
     for (int i = 0; i <= (N); i++)
     {
-      res[i] = u(x[i]);
+    // updating
+    res[i] = u(x[i]);
     // Write a line with the current x and y values (nicely formatted) to file
     ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x[i]
           << std::setw(width) << std::setprecision(prec) << std::scientific << res[i]
---
+++ Project1/p2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('p2.txt')
+
+x = data[:, 0] # x - vector
+u_x = data[:, 1] # u(x) values
+
+plt.plot(x, u_x)
+plt.title(r"$u(x)=1-(1-e^{-10})x-e^{-10x}$")
+plt.xlabel(r"$x\in[0, 1]$")
+plt.ylabel(r"$u(x)$")
+plt.savefig("p2.png")
