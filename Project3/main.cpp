43c025a05c1d5ab99e1779b45505de41da4738e9
larsfop <lars.opgaard@hotmail.com>
2022-10-04 11:40:31 +0200

    small changes

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,4 +1,4 @@
-#include "Particle.hpp"
+#include "PenningTrap.hpp"
 
 int main()
 {
@@ -6,6 +6,18 @@ int main()
   Particle particle_1 = Particle(1.0, 0.2, arma::vec("1 2 3"), arma::vec("3 2 1"));
   // printing info to test
   std::cout << particle_1.info();
+
+  PenningTrap trap = PenningTrap(96.5, 9.65*std::pow(10,8), std::pow(10,4));
+  trap.add_particle(particle_1);
+
+  Particle particle_2 = Particle(1.0, 0.1, arma::vec("3 2 1"), arma::vec("1 2 3"));
+  trap.add_particle(particle_2);
+
+  arma::vec r_test,ext_force,part_force;
+  //r_test = trap.external_B_field(particle_1.position());
+  r_test = trap.total_force(0);
+  r_test.print(std::cout);
+
 
   return 0;
 }
